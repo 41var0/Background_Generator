cc5a7a2fcaf969e1a75f6fe2326f731f1c2627e2
Álvaro Cordero Fernández <alvarocorderof06@gmail.com>
2026-09-17 03:41:40 +0200

    minor changesVerified method [FINISHED]
    - Method: "triangle_from_vertical_lines": It makes a triangle from vertical lines, painted with pixels (of 2x2px actually)
    - The triangle can be flipped horizontally or vertically. Can be painted with any color.
    - The method at least will need the Draw object of the image (background). At consideration if it could take just the Image object (instead of Draw) on the future.
    - It's a simple method, but it will work as a trigger for more.

--- Experimental/image_creation/verified_methods_exp.py
+++ Experimental/image_creation/verified_methods_exp.py
@@ -2,67 +2,51 @@ from PIL.ImageDraw import ImageDraw
 
 
 
-def triangle_from_vertical_lines(color:tuple, draw:ImageDraw, reverse:bool = False, upsidedown:bool = False):
+def triangle_from_vertical_lines(draw:ImageDraw, color:tuple=(255, 255, 255), flip_h:bool = False, flip_v:bool = False):
     """
-    Draws a triangle with vertical lines. Original orientation is left to right on decrease:
-    :param reverse: Makes the triangle increase throughout width
-    :param upsidedown: Makes the triangle turn upside-down
-    :param color: Color of teh triangle
+    Draws a triangle with vertical lines. Original orientation is left to right increassing:
+    :param flip_h: Makes the triangle turn arround horizontally
+    :param flip_v: Makes the triangle turn arround vertically
+    :param color: Color of the triangle
     :param draw: ImageDraw object of the image
     """
     # Usage examples:
-    # triangle_from_vertical_lines(draw=draw, color=(55, 255, 255))
-    # triangle_from_vertical_lines(draw=draw, color=(55, 255, 255), reverse=True)
-    # triangle_from_vertical_lines(draw=draw, color=(55, 255, 255), upsidedown=True)
-    # triangle_from_vertical_lines(draw=draw, color=(55, 255, 255), reverse=True, upsidedown=True)
+    # triangle_from_vertical_lines(draw=draw)
+    # triangle_from_vertical_lines(draw=draw, color=color)
+    # triangle_from_vertical_lines(draw=draw, color=color, flip_h=True)
+    # triangle_from_vertical_lines(draw=draw, color=color, flip_v=True)
+    # triangle_from_vertical_lines(draw=draw, color=color, flip_h=True, flip_v=True)
 
     original_width, original_height = draw.im.size
 
     relation = (original_height / original_width)    # Relation between variables we're gonna iterate
-    relation *= 4                  # is 4* because of pixels with movement
-    orden = 1
+    relation *= 4                                    # is 4* because of pixels with movement
 
 
-    if (not reverse):
+    # Fliping the image H and V makes them having some common variables
+    if (not flip_v):
+        min_triangle_height = original_height
+        vertical_steps = -2
+    else:
+        min_triangle_height = 0
+        vertical_steps = 2
+
+
+    if (not flip_h != flip_v):
         max_triangle_height = original_height
     else:
         max_triangle_height = 0
         relation *= -1
 
 
-    if (not upsidedown):
-        min_triangle_height = 0
-        vertical_steps = 2
-    else:
-        min_triangle_height = original_height
-        max_triangle_height = original_height
-        vertical_steps = -2  # vertical steps its 2 because of "pixels width"
-        relation *= -1
-
-    # at the end the relation needs to be negative or positive in order to
-    #  increase or decrease the max_triangle_height: -*-=+ or -*+=-
-    # relation *= orden
-
-
-
-
-
-
-
-    print(f"Condiciones iniciales de max_triangle_height {max_triangle_height}")
-    print(f"Condiciones iniciales de min_triangle_height {min_triangle_height}")
-    print(f"Condiciones iniciales de vertical_steps {vertical_steps}")
-    print(f"Condiciones iniciales de relation {relation}")
-
-
     for w in range(0, original_width, 4):
         for h in range(min_triangle_height, int(max_triangle_height), vertical_steps):
 
+            # "pixel" of 2x2 pixels
             draw.rectangle(xy=((w, h), (w + 1, h + 1)), outline=color)
             # Differece between methods :
             # img.putpixel((w+1, h), (55, 255, 255))   : Time take 0:00:00.517718
             # draw.rectangle(xy=((w, h), (w+1, h+1)))  : Time take 0:00:00.106540
-
 
         max_triangle_height = max_triangle_height - relation
 
